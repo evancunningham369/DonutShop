adb0eff46c659e32e688d627cb34a735b79e35e8
evancunningham369 <evancunningham369@yahoo.com>
2024-11-24 11:44:07 -0600

    retrieve donuts from backend instead of hardcode

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,79 +1,62 @@
 import Header from "./Header";
 import Footer from './Footer';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { addToUserCart, logOut } from "../fetch_req";
+import { logout, getAllDonuts } from "../fetch_req";
 import { Card } from "./Card";
 
 function Home(){
-    const user = JSON.parse(sessionStorage.getItem('user'));
-    const [checkedOut, setCheckout] = useState(false);
+    const username = JSON.parse(sessionStorage.getItem('username'));
+    const userId = JSON.parse(sessionStorage.getItem('userId'));
+    const [donuts, setDonuts] = useState([]);
     const navigate = useNavigate();
 
-    const donuts = [
-        {
-            name: "Glazed",
-            image: 'https://images.unsplash.com/photo-1564545508148-9f5ce263df8e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Z2xhemVkJTIwZG9udXR8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60',
-            desc: 'A donut covered in sweet glaze',
-            price:1.00,
-        },
-        {
-            name: "Cake",
-            image: 'https://cdn.pixabay.com/photo/2017/04/04/16/35/cake-2201820_1280.jpg',
-            desc: 'A whole cake you can fit in the palm of your hand!',
-            price:2.00,
-        },
-        {
-            name: "Plain",
-            image: 'https://static4.depositphotos.com/1006913/269/i/600/depositphotos_2696137-stock-photo-donut.jpg',
-            desc: 'Simply a traditional plain donut. I won\'t judge....much',
-            price:0.50,
-        },
-        {
-            name: "Chocolate Sprinkle",
-            image: 'https://images.unsplash.com/photo-1562945431-ce2b63d5a7fe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80',
-            desc: 'What\'s better than a chocolate donut? A chocolate donut...with sprinkles',
-            price:1.50,
-        },];
+    useEffect(() => {
+        const fetchDonuts = async() => {
+            try {
+                const response = await getAllDonuts(userId);
+                
+                const data = await response.json();
+                
+                setDonuts(data);
+            } catch (error) {
+                console.error("Error:", error);
+            }
+        }
+
+        fetchDonuts();
+    }, [userId])
 
     async function checkOut(){
-        setCheckout(true);
-        await new Promise(resolve => setTimeout(resolve, 500));
         navigate('../Checkout');
     }
 
-    async function handleLogOut(){
-        const response = await logOut(user.username);
+    async function handleLogout(){
+        const response = await logout(username);
+        
         sessionStorage.clear();
         if(response.ok){
-            navigate('../');
+            navigate('/');
         }
     }
-
-    async function addDonutToCart(donut){
-        sessionStorage.setItem(donut.name, JSON.stringify(donut.quantity));
-        await addToUserCart(user.username, donut);
-    }
-
+    
     return(
         <div>
             <Header />
-            {checkedOut ? (<h1>Loading cart...</h1>): 
-            (<div className="d-flex flex-column">
-                    <h2>Logged in as {user.username}</h2>
-                <div className="card-group mt-2">
-                    {donuts.map((donut, index) => {
-                        const quantity = sessionStorage.getItem(donut.name);
-                        return (
-                            <Card addToCart={(donut) => addDonutToCart(donut)} key={index} name={donut.name} quantity={quantity} price={donut.price} desc={donut.desc} image={donut.image}/>
-                        )
-                    })}
+                <div className="d-flex flex-column">
+                        <h2>Logged in as {username}</h2>
+                    <div className="card-group mt-2">
+                        {
+                            donuts.map(donut =>
+                                <Card key={donut.donut_id} userId={userId} donutId={donut.donut_id} name={donut.name} price={donut.price} quantity={donut.quantity}/>
+                            )
+                        }
+                    </div>
+                    <div>
+                        <button onClick={checkOut} className="btn btn-primary">Go To Cart</button>
+                        <button onClick={handleLogout} className="btn btn-primary">Log-Out</button>
+                    </div>
                 </div>
-                <div>
-                    <button onClick={checkOut} className="btn btn-primary">Go To Cart</button>
-                    <button onClick={handleLogOut} className="btn btn-primary">Log-Out</button>
-                </div>
-            </div>)}
             <Footer />
         </div>
     );
