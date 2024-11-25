62a66ce07403815735734f4bb288d3626db6f9ce
evancunningham369 <evancunningham369@yahoo.com>
2024-11-25 10:17:42 -0600

    edit bootstrap classes to increase responsiveness

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -14,19 +14,19 @@ export function Card(props){
     }
 
     return (
-        <div className="col-lg-2 d-flex align-items-stretch">
+        <div className="col">
             <div className="card">
-                <img className="card-img-top" src={props.image} alt={props.name + " Donut image"} />
-                <div className="card-body d-flex flex-column">
-                    <h5 className="card-title">{props.name + " Donut"}</h5>
-                    <p className="card-text">description</p>
-                    <div className="row mt-auto">
+                <h5 className="card-title">{props.name + " Donut"}</h5>
+                <h5>Price: ${props.price}</h5>
+                {/* <p className="card-text">description</p> */}
+                <div className="card-body d-flex align-items-center">
+                    <img className="card-img-top card-image" src={`/images/${props.name}.jpg`} alt={props.name + " Donut image"} />
+                </div>
+                    <div className="quantity-buttons">
                         <button onClick={() => updateQuantity(quantity - 1)} className="btn btn-primary btn-circle col-md-4" disabled={quantity === 0}>-</button>
+                        <h2>Qty: {quantity}</h2>
                         <button onClick={() => updateQuantity(quantity + 1)} className="btn btn-primary btn-circle col-md-4">+</button>
                     </div>
-                        <h3>Price: ${props.price}</h3>
-                    <h2>Qty: {quantity}</h2>
-                </div>
             </div>
         </div>
     )
