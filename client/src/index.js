13b2bf6ac0108ee76f556b0ab16292a421bc30e8
evancunningham369 <evancunningham369@yahoo.com>
2025-08-16 02:16:26 -0500

    Wrap App component with BrowserRouter and UserProvider for routing and context management

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import { BrowserRouter } from 'react-router-dom';
+import { UserProvider } from './context/UserContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <BrowserRouter>
+        <UserProvider>
+            <App />
+        </UserProvider>
+    </BrowserRouter>
 );
