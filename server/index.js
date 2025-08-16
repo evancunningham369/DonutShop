fec83a4ecdf813f7dc925c8ecd4f13e1af83bd20
evancunningham369 <evancunningham369@yahoo.com>
2025-08-16 02:17:26 -0500

    Refactor server setup to include session management with PostgreSQL and improve CORS configuration

--- server/index.js
+++ server/index.js
@@ -1,16 +1,50 @@
 import express from 'express';
 import cors from 'cors';
+import pool from './config/database.js';
+
+import session from 'express-session';
+import pgSimple from 'connect-pg-simple';
 
 import donutRoutes from './routes/donutRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 import cartRoutes from './routes/cartRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const ORIGIN = 'http://localhost:3000';
 
-app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
+
+app.use(cors({
+    origin: ORIGIN,
+    credentials: true,
+}));
+
+// Postgres store ----
+const PgSession = pgSimple(session);
+
+app.use(
+    session({
+        store: new PgSession({
+            pool,
+            tableName: 'session',
+            createTableIfMissing: true,
+        }),
+        name: process.env.COOKIE_NAME || 'sid',
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'lax',
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+        },
+    })
+);
 
 app.use('/donuts', donutRoutes);
 app.use('/auth', authRoutes);
