aafa25d06611f3c471d1a6b0bc6deb5cb9438d76
evancunningham369 <evancunningham369@yahoo.com>
2025-08-16 02:17:11 -0500

    Refactor Home component to use UserContext for user data and remove sessionStorage

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,19 +4,17 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { logout, getAllDonuts } from "../fetch_req";
 import { Card } from "./Card";
+import { useUser } from "../context/UserContext";
 
 function Home(){
-    const username = JSON.parse(sessionStorage.getItem('username'));
-    const userId = JSON.parse(sessionStorage.getItem('userId'));
     const [donuts, setDonuts] = useState([]);
     const navigate = useNavigate();
-    
-    useEffect(() => {
-        
+    const { user, setUser } = useUser();
 
+    useEffect(() => {
         const fetchDonuts = async() => {
             try {
-                const data = await getAllDonuts(userId);
+                const data = await getAllDonuts(user.userId);
                 setDonuts(data);
             } catch (error) {
                 console.error("Error:", error);
@@ -24,7 +22,7 @@ function Home(){
         }
 
         fetchDonuts();
-    }, [userId])
+    }, [user.userId])
 
     async function checkOut(){
         navigate('../Checkout');
@@ -32,19 +30,18 @@ function Home(){
 
     async function handleLogout(){
         await logout();
-        sessionStorage.clear();
-        navigate('/');
+        setUser(null);
     }
     
     return(
         <div>
             <Header />
             <div className="container">
-                <h2>Logged in as {username}</h2>
+                <h2>Logged in as {user.username}</h2>
                 <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 g-3">
                     {
                             donuts.map(donut =>
-                                <Card key={donut.donut_id} userId={userId} donutId={donut.donut_id} name={donut.name} price={donut.price} quantity={donut.quantity}/>
+                                <Card key={donut.donut_id} userId={user.userId} donutId={donut.donut_id} name={donut.name} price={donut.price} quantity={donut.quantity}/>
                             )
                     }
                 </div>
