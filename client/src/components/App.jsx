83449bfcd81d2c6afd599d5f7b1e55098eeb34a2
evancunningham369 <evancunningham369@yahoo.com>
2025-08-16 02:16:32 -0500

    Refactor App component to remove BrowserRouter and integrate user-based routing logic

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,22 +1,21 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Register from './Register';
 import Home from './Home';
 import Users from './Users';
 import Checkout from './Checkout';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useUser } from '../context/UserContext.js';
 
-function App() {
+export default function App() {
+  const { user } = useUser();
+
+  if (user === undefined) return <div>Loading...</div>
+
   return (
-    <BrowserRouter>
-      <main>
-        <Routes>
-          <Route path = '/' element={<Register />}/>
-          <Route path = '/home' element={<Home/>}/>
-          <Route path = '/users' element={<Users />}/>
-          <Route path = '/checkout' element={<Checkout />}/>
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <Routes>
+      <Route path = '/'         element={user ? <Navigate to='/home' replace /> : <Register />}/>
+      <Route path = '/home'     element={user ? <Home/> : <Navigate to='/' replace />}/>
+      <Route path = '/users'    element={<Users />}/>
+      <Route path = '/checkout' element={user ? <Checkout /> : <Navigate to='/' replace /> }/>
+    </Routes>
   );
 }
-
-export default App;
